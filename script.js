dcfc9c82b0086e639737f69eed1210a842925fdd
Christopher Orth <christopherorth430@yahoo.com>
2023-08-16 20:06:51 -0400

    final touches

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const input = document.getElementById('button');
+
 input.addEventListener('click', function() {
-    alert('I really appreciate you coming and visiting this page and taking the time to learn a little about me.');
+    alert('I really appreciate you coming to visit this page and taking the time to learn a little about me.');
     alert('Hope you have an amazing day!');
 });
